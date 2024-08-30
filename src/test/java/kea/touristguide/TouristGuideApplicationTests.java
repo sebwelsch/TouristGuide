3c11c95da3c0118ea381b16e0f91cfe783bae58c
Sebastian <sebjak1@hotmail.com>
2024-08-30 15:11:40 +0200

    initial commit

---
+++ src/test/java/kea/touristguide/TouristGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.touristguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
