294e9a00c9116a60d143428034e093706a8f5b3a
Sebastian <sebjak1@hotmail.com>
2024-09-02 21:25:18 +0200

    start

--- src/main/java/kea/touristguide/controller/TouristController.java
+++ src/main/java/kea/touristguide/controller/TouristController.java
@@ -1,4 +1,13 @@
 package kea.touristguide.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/attractions")
 public class TouristController {
+
+    public TouristController() {
+
+    }
 }
--- src/main/java/kea/touristguide/repository/TouristRepository.java
+++ src/main/java/kea/touristguide/repository/TouristRepository.java
@@ -1,4 +1,8 @@
 package kea.touristguide.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class TouristRepository {
+
 }
