d42133bda028b63f67c4133375768eae28160fa7
Sebastian <sebjak1@hotmail.com>
2024-10-23 16:16:31 +0200

    workflow

--- src/main/java/kea/touristguide/controller/TouristController.java
+++ src/main/java/kea/touristguide/controller/TouristController.java
@@ -3,7 +3,6 @@ package kea.touristguide.controller;
 import kea.touristguide.model.TouristAttraction;
 import kea.touristguide.service.TouristService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
