18a9beaa07d4e75266847931dca8cc7575e1b962
Sebastian <sebjak1@hotmail.com>
2024-10-23 12:49:30 +0200

    feature: database env variables

--- src/main/java/kea/touristguide/repository/TouristRepository.java
+++ src/main/java/kea/touristguide/repository/TouristRepository.java
@@ -1,8 +1,10 @@
 package kea.touristguide.repository;
 
 import kea.touristguide.model.TouristAttraction;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -13,12 +15,22 @@ import java.util.TreeSet;
 @Repository
 public class TouristRepository {
     private ArrayList<TouristAttraction> touristAttractions = new ArrayList<>();
+    @Value("${spring.datasource.url}")
+    private String db_url;
+    @Value("${spring.datasource.username}")
+    private String db_username;
+    @Value("${spring.datasource.password}")
+    private String db_password;
 
     public TouristRepository() {
         touristAttractions.add(new TouristAttraction("SMK", "Museum for Kunst", "København", List.of("Kunst", "Museum")));
         touristAttractions.add(new TouristAttraction("Odense Zoo", "Europas bedste zoo", "Odense", List.of("Børnevenlig")));
         touristAttractions.add(new TouristAttraction("Dyrehaven", "Naturpark med skovområder", "Kongens Lyngby", List.of("Natur", "Gratis")));
         touristAttractions.add(new TouristAttraction("Tivoli", "Forlystelsespark i Københavns centrum", "København", List.of("Børnevenlig")));
+        try (Connection dbConnection = DriverManager.getConnection(db_url, db_username, db_password)) {
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public ArrayList<TouristAttraction> getAllTouristAttractions() {
